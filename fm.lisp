816a847d083ec8d45324e1fb6eea230ee2645488
Martin Kielhorn <kielhorn.martin@googlemail.com>
2012-04-06 10:58:34 +0200

    use napa-fft3 and shift so that result looks like in baudline

--- fm.lisp
+++ fm.lisp
@@ -1,21 +1,110 @@
+(ql:quickload "napa-fft3")
+
+(defun next-power-of-two (n)
+  (let ((p 2))
+   (loop while (< p n) do 
+	(setf p (* 2 p)))
+   p))
+#+nil
+(next-power-of-two 33)
+
+(defun store-sfloat (fn seq)
+  (with-open-file (s fn
+		     :direction :output
+		    :element-type '(unsigned-byte 8)
+		    :if-does-not-exist :create
+		    :if-exists :supersede)
+   (let* ((a (sb-alien:sap-alien (sb-sys:vector-sap seq)
+				 (sb-alien:* sb-alien:unsigned-char)))
+	  (n (length seq))
+	  (o (make-array (* n 4) :element-type '(unsigned-byte 8))))
+     (dotimes (i (* 4 n))
+       (setf (aref o i) (deref a i)))
+     (write-sequence o s))))
+
+(defun store-dfloat (fn seq)
+  (with-open-file (s fn
+		     :direction :output
+		    :element-type '(unsigned-byte 8)
+		    :if-does-not-exist :create
+		    :if-exists :supersede)
+   (let* ((a (sb-alien:sap-alien (sb-sys:vector-sap seq)
+				 (sb-alien:* sb-alien:unsigned-char)))
+	  (n (length seq))
+	  (o (make-array (* n 8) :element-type '(unsigned-byte 8))))
+     (dotimes (i (* 8 n))
+       (setf (aref o i) (deref a i)))
+     (write-sequence o s))))
 
 (time
- (defparameter *input*
-   (let* ((n 63438848)
-	  (a (make-array n :element-type '(unsigned-byte 8)))
-	  (c (make-array (floor n 2) :element-type '(complex single-float))))
-     (with-open-file (s "/home/martin/Downloads2/dlf.fm"
-			:element-type '(unsigned-byte 8))
-       (read-sequence a s))
-     (dotimes (i (floor n 2))
-       (let ((x  (aref a (* 2 i)))
-	     (y  (aref a (1+ (* 2 i)))))
-	 (when (= x y)
-	   (incf x .5))
-	 (setf (aref c i) 
-	       (complex (- x 127f0)
-			(- y 127f0)))))
-     c)))
+ (progn
+  (defparameter *rate* 2048000)
+  (defparameter *n-complex* (floor (expt 2 16) #+nil 47448064 2))
+  (defparameter *input*
+    (let* ((n (* 2 *n-complex*))
+	   (a (make-array n :element-type '(unsigned-byte 8)))
+	   (c (make-array (next-power-of-two (floor n 2))
+			  :element-type '(complex double-float))))
+      (with-open-file (s "/home/martin/Downloads2/dl2048000.fm"
+			 :element-type '(unsigned-byte 8))
+	(read-sequence a s))
+      (dotimes (i (floor n 2))
+	(let ((x  (aref a (* 2 i)))
+	      (y  (aref a (1+ (* 2 i)))))
+	  (when (= x y)
+	    (incf x .5))
+	  (setf (aref c i) 
+		(* #+nil (exp (complex 0d0 (* (/ (* 2d0 pi) *n-complex*)
+					i)))
+		   (complex (- x 127d0)
+			    (- y 127d0))))))
+      c))))
+
+
+(defun fftshift (a)
+  (let* ((n (length a))
+	 (nh (floor n 2))
+	 (b (make-array n
+		       :element-type (array-element-type a)
+		       :initial-element (complex .001d0))))
+    (unless (evenp n)
+      (error "n must be even."))
+    (dotimes (i nh)
+      (setf (aref b i) (aref a (- nh 1 i))
+	    (aref b (+ nh i)) (aref a (- n 1 i))))
+    b))
+
+
+(time
+ (defparameter *kin*
+   (fftshift
+    (napa-fft:fft *input*))))
+
+
+(time
+ (let* ((nn (length *kin*))
+       (all (make-array nn
+			:element-type 'double-float))
+       (m (map-into all #'abs *kin*))
+       (m2 (map-into all #'log all))
+       (n 1000)
+       (big-bins (floor nn
+			n))
+       (res (make-array n :element-type 'double-float
+			:initial-element 0d0)))
+  (defparameter *abs-kin* all
+#+nil
+    (dotimes (i n)
+      (dotimes (b big-bins)
+	(let ((p (+ b (* 1000 i))))
+	  (when (< p nn)
+	    (incf (aref res i) (aref all p)))))))
+  (with-open-file (s "/dev/shm/o.dat"
+		    :direction :output
+		    :if-does-not-exist :create
+		    :if-exists :supersede)
+   (loop for i from (- (floor nn 2)) and e across *abs-kin* do
+	(format s "~7,3f ~7,3f~%" (/ (* *rate* i) *n-complex*) e)))))
 
 ;; s = A e^ip
 ;; ds/dt = d/dt A e^ip = A ip' e^ip
@@ -54,19 +143,6 @@
 #+nil
 (sb-ext:gc :full t)
 
-(defun store-sfloat (fn seq)
-  (with-open-file (s fn
-		     :direction :output
-		    :element-type '(unsigned-byte 8)
-		    :if-does-not-exist :create
-		    :if-exists :supersede)
-   (let* ((a (sb-alien:sap-alien (sb-sys:vector-sap seq)
-				 (sb-alien:* sb-alien:unsigned-char)))
-	  (n (length seq))
-	  (o (make-array (* n 4) :element-type '(unsigned-byte 8))))
-     (dotimes (i (* 4 n))
-       (setf (aref o i) (deref a i)))
-     (write-sequence o s))))
 
 #+nil
 (let* ((n  (* 10 1024)) ;; verify data export with a sine wave
@@ -78,3 +154,4 @@
 
 (time
  (store-sfloat "/dev/shm/demod.sfload" *demod*))
+
